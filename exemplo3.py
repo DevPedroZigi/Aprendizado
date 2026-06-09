fb68e779ae80b5123505fc74f68c8b5c4586cf6c
Dev Pedro Zigi <kyzampedro@gmail.com>
2026-06-09 20:51:35 -0300

    .

--- exemplo3.py
+++ exemplo3.py
@@ -4,3 +4,9 @@ def filtrar_pares(lista):
         if item % 2 == 0:
             lista_pares.append(item)
     return lista_pares
+
+
+# programa principal
+lista = [1 ,4 ,7, 10, 13, 16, 19, 22]
+lista_pares = filtrar_pares(lista)
+print(lista_pares)
---
+++ exemplo5.py
@@ -0,0 +1,46 @@
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("Erro: Divisão por zero não é permitida.")
+        return None
+
+def exibir_menu():
+    while True:
+        print("1 - Soma")
+        print("2 - Subtração")
+        print("3 - Multiplicação")
+        print("4 - Divisão")
+        print("5 - Sair")
+        opcao = int(input("Escolha uma opção: "))
+        a = float(input("Digite o primeiro número: "))
+        b = float(input("Digite o segundo número: "))
+        if opcao == 1:
+            print(f"O resultado da soma é: {somar(a, b)}")
+        elif opcao == 2:
+            print(f"O resultado da subtração é: {subtrair(a, b)}")
+        elif opcao == 3:
+            print(f"O resultado da multiplicação é: {multiplicar(a, b)}")
+        elif opcao == 4:
+            resultado = dividir(a, b)
+            if resultado is not None:
+                print(f"O resultado da divisão é: {resultado}")
+        elif opcao == 5:
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+
+# programa principal
+exibir_menu()
