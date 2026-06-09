547b46cbf37b86313e97015b69bd35eb3630c958
Dev Pedro Zigi <kyzampedro@gmail.com>
2026-06-09 19:55:44 -0300

    revisando

---
+++ exemplo2.py
@@ -0,0 +1,9 @@
+def calcular_area_retangulo(base, altura):
+    area = base * altura
+    print(area)
+
+
+# programa principal
+base = float(input("Informe a medida da base do retângulo: "))
+altura = float(input("informe a medida da altura do retângulo: "))
+calcular_area_retangulo(base, altura)
