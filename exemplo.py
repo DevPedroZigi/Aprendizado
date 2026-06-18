004ea6eb7e8e2a8d48086564ac532ba272b370a3
Dev Pedro Zigi <kyzampedro@gmail.com>
2026-06-18 15:06:19 -0300

    .

--- exemplo.py
+++ exemplo.py
@@ -1,14 +1,10 @@
-import time
+dia = 0
 
-def exibir_mensagem(texto, tempo):
-  for i in range(tempo, 0 ,-1):
-    print(f"{i}")
-    time.sleep(1)
-  print(texto)
-
-
-# programa principal
-msg = input("Digite a mensagem a ser exibida: ")
-exibir_mensagem(msg, 5)
-
-def 
+while True:
+    if dia <= 100:
+        print("AGAIN!")
+    else:
+        print("CONGRATULATIONS!")
+        break
+    dia += 1
+    
